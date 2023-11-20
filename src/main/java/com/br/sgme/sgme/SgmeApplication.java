f917827e75555afc9b3699f54ce75f0055e34412
Poliana Lima <pollyannati@hotmail.com>
2023-11-20 10:11:50 -0300

    adicionando usuario repository para context spring

--- src/main/java/com/br/sgme/sgme/SgmeApplication.java
+++ src/main/java/com/br/sgme/sgme/SgmeApplication.java
@@ -1,9 +1,13 @@
 package com.br.sgme.sgme;
 
+
+import com.br.sgme.sgme.repository.UsuarioRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {
+        UsuarioRepository.class
+})
 public class SgmeApplication {
 
     public static void main(String[] args) {
