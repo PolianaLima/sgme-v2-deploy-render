64135443deade2a0c34c1a35bafff217ae96f89e
Poliana Lima <pollyannati@hotmail.com>
2023-11-20 08:36:18 -0300

    Alterando exclude DataSource

--- src/main/java/com/br/sgme/sgme/SgmeApplication.java
+++ src/main/java/com/br/sgme/sgme/SgmeApplication.java
@@ -2,8 +2,9 @@ package com.br.sgme.sgme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SgmeApplication {
 
     public static void main(String[] args) {
