3ebcb2dd6307d79979f4079b1ed411db9fb16dd5
Poliana Lima <pollyannati@hotmail.com>
2023-11-20 13:59:43 -0300

    comentando teste unit

--- src/test/java/com/br/sgme/sgme/SgmeApplicationTests.java
+++ src/test/java/com/br/sgme/sgme/SgmeApplicationTests.java
@@ -1,7 +1,7 @@
 package com.br.sgme.sgme;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
 class SgmeApplicationTests {
